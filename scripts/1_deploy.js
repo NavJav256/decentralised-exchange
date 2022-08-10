d5f970309fa4c2aeff5638948d80ad0626e7ad98
NavJav256 <55883312+NavJav256@users.noreply.github.com>
2022-08-10 19:34:17 +0100

    Add some background data to the exchange

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,12 +1,36 @@
 async function main() {
+
+  console.log('Deploying ...')
+
   const Token = await ethers.getContractFactory('Token')
-  const token = await Token.deploy()
-  await token.deployed()
-  console.log(`Token deployed to: ${token.address}`)
+  const Exchange = await ethers.getContractFactory('Exchange')
+
+  const accounts = await ethers.getSigners()
+  console.log('Fetching accounts: ')
+  accounts.forEach(account => {
+    console.log(account.address)
+  })
+
+  const navT = await Token.deploy('Nav Token', 'NAVT', 1000000)
+  await navT.deployed()
+  console.log(`Nav Token deployed to: ${navT.address}`)
+
+  const mETH = await Token.deploy('Mock Eth', 'mETH', 1000000)
+  await mETH.deployed()
+  console.log(`Mock Eth deployed to: ${mETH.address}`)
+
+  const mDAI = await Token.deploy('Mock Dai', 'mDAI', 1000000)
+  await mDAI.deployed()
+  console.log(`Mock Dai deployed to: ${mDAI.address}`)
+
+  const exchange = await Exchange.deploy(accounts[1].address, 10)
+  await exchange.deployed()
+  console.log(`Exchange deployed to: ${exchange.address}`)
+
 
 }
 
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  console.error(error)
+  process.exitCode = 1
+})
---
+++ scripts/2_seed-exchange.js
@@ -0,0 +1,128 @@
+const config = require('../src/config.json')
+
+const convertToEther = (n) => {
+    return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
+const wait = (seconds) => {
+    const milliseconds = seconds * 1000
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+async function main() {
+
+    //Get all accounts
+    const accounts = await ethers.getSigners()
+
+    const { chainId }  = await ethers.provider.getNetwork()
+
+    //Initialise tokens
+    const navT = await ethers.getContractAt('Token', config[chainId].navT.address)
+    const mETH = await ethers.getContractAt('Token', config[chainId].mETH.address)
+    const mDAI = await ethers.getContractAt('Token', config[chainId].mDAI.address)
+
+    //Initialise exchange
+    const exchange = await ethers.getContractAt('Exchange', config[chainId].exchange.address)
+
+    //Give tokens to account 1
+    const sender = accounts[0]
+    const receiver = accounts[1]
+    let amount = convertToEther(10000)
+
+    let transaction, result
+
+    transaction = await mETH.connect(sender).transfer(receiver.address, amount)
+
+    //Set up exchange users
+    const user1 = accounts[0]
+    const user2 = accounts[1]
+
+    //User 1 approves 10,000 nav tokens
+    transaction = await navT.connect(user1).approve(exchange.address, amount)
+    await transaction.wait()
+
+    //User 1 deposits 10,000 nav tokens
+    transaction = await exchange.connect(user1).depositToken(navT.address, amount)
+    await transaction.wait()
+
+    //User 2 approves 10,000 mETH tokens
+    transaction = await mETH.connect(user2).approve(exchange.address, amount)
+    await transaction.wait()
+
+    //User 2 deposits 10,000 mETH tokens
+    transaction = await exchange.connect(user2).depositToken(mETH.address, amount)
+    await transaction.wait()
+
+    //User 1 makes an order
+    let orderID
+    transaction = await exchange.connect(user1).makeOrder(mETH.address, convertToEther(100), navT.address, convertToEther(5))
+    result = await transaction.wait()
+
+    //User 1 cancels the order
+    orderID = result.events[0].args._id
+    transaction = await exchange.connect(user1).cancelOrder(orderID)
+    result = await transaction.wait()
+
+    //Wait 1 second
+    await wait(1)
+
+    //User 1 makes another order
+    transaction = await exchange.connect(user1).makeOrder(mETH.address, convertToEther(100), navT.address, convertToEther(10))
+    result = await transaction.wait()
+
+    //User 2 fills the order
+    orderID = result.events[0].args._id
+    transaction = await exchange.connect(user2).fillOrder(orderID)
+    result = await transaction.wait()
+
+    //Wait 1 second
+    await wait(1)
+
+    //User 1 makes another order
+    transaction = await exchange.connect(user1).makeOrder(mETH.address, convertToEther(50), navT.address, convertToEther(15))
+    result = await transaction.wait()
+
+    //User 2 fills the order
+    orderID = result.events[0].args._id
+    transaction = await exchange.connect(user2).fillOrder(orderID)
+    result = await transaction.wait()
+
+    //Wait 1 second
+    await wait(1)
+
+    //User 1 makes another order
+    transaction = await exchange.connect(user1).makeOrder(mETH.address, convertToEther(200), navT.address, convertToEther(20))
+    result = await transaction.wait()
+
+    //User 2 fills the order
+    orderID = result.events[0].args._id
+    transaction = await exchange.connect(user2).fillOrder(orderID)
+    result = await transaction.wait()
+
+    //Wait 1 second
+    await wait(1)
+
+    //User 1 makes 10 random orders
+    for(let i=1; i<=10; i++) {
+        var randNumOne = Math.floor(Math.random() * 10) + 1 
+        var randNumTwo = Math.floor(Math.random() * 10) + 1
+        transaction = await exchange.connect(user1).makeOrder(mETH.address, convertToEther(randNumOne * i), navT.address, convertToEther(randNumTwo * i / 2))
+
+        await wait(1)
+    }
+
+    //User 2 makes 10 random orders
+    for(let i=1; i<=10; i++) {
+        var randNumOne = Math.floor(Math.random() * 10) + 1 
+        var randNumTwo = Math.floor(Math.random() * 10) + 1
+        transaction = await exchange.connect(user2).makeOrder(navT.address, convertToEther(randNumOne * i / 2), mETH.address, convertToEther(randNumTwo * i))
+
+        await wait(1)
+    }
+
+}
+
+main().catch((error) => {
+    console.error(error)
+    process.exitCode = 1
+  })
